89c4acfd3d26aa68c2c7c2680dd05c70f962e3b4
spacekey <fumihiro.fukuda@gmail.com>
2018-07-17 08:33:53 +0800

    Change ad unit.

--- src/components/ad.jsx
+++ src/components/ad.jsx
@@ -4,11 +4,14 @@ export default function Ad() {
     return (
         <div className="ad">
             <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-            <ins className="adsbygoogle"
-                style={{ display: "inline-block", width: "468px", height:"60px"}}
+            <ins class="adsbygoogle"
+                style="display:block"
                 data-ad-client="ca-pub-4343024996209944"
-                data-ad-slot="6580321139"></ins>
-            <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+                data-ad-slot="2071710325"
+                data-ad-format="auto"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
         </div>
     );
 }
